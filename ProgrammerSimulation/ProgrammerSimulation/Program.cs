31b1940551164c3ad47978a4e1df4d5dde69c747
RyanB <ryan.m.blakley@gmail.com>
2018-02-12 11:49:55 -0600

    Change Project

---
+++ ProgrammerSimulation/ProgrammerSimulation/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ProgrammerSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets get started!");
+
+            String question;
+            String answer;
+            String correct;
+
+            question = "Declare a variable called x of type Integer (Don't forget your semicolons!)";
+            answer = "intx;";
+            correct = "Looks good! Here comes your next challenge";
+
+            Question one = new Question(question, answer, correct, 3);
+
+            one.askQuestion();
+
+            question = "Assign 3 to the variable you defined above.";
+            answer = "x=3;";
+            correct = "Variable defined.";
+
+            Question two = new Question(question, answer, correct, 3);
+            two.askQuestion();
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ProgrammerSimulation/ProgrammerSimulation/Question.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProgrammerSimulation
+{
+    class Question
+    {
+        /**
+         * tries: the amount of tries the user has to correctly answer the question for points.
+         * points: a dynami value of how many points will be given once the question is answered correctly.
+         * question: the question to be asked.
+         * answer: the exact syntax of the correct answer.
+         * **Remove all empty spaces from the answer before setting it.
+         * correct: the correct
+         */
+
+        private int _tries;
+        private int _points;
+
+        private String _question;
+        private String _answer;
+        private String _correct;
+
+        private Boolean isCorrect = false;
+
+        public Question(String question, String answer, String correct, int tries)
+        {
+            if (tries <= 0)
+            {
+                _tries = 1;
+            }
+            else
+            {
+                _tries = tries;
+            }
+
+            _question = question;
+            _answer = answer;
+            _correct = correct;
+            _points = _tries;
+        }
+
+        public void askQuestion()
+        {
+            isCorrect = false;
+
+            while (!isCorrect)
+            {
+                Console.WriteLine(_question);
+                String entry = Console.ReadLine();
+
+                Console.Write("{0,24}", "You entered \"" + entry + "\" ");
+
+                entry = entry.Replace(" ", String.Empty);
+
+                if (System.Text.RegularExpressions.Regex.IsMatch(entry, _answer, System.Text.RegularExpressions.RegexOptions.IgnoreCase))
+                {
+                    Console.Write(_correct);
+
+                    Console.WriteLine();
+                    isCorrect = true;
+                }
+                else
+                {
+                    Console.WriteLine();
+                }
+            }
+
+
+        }
+    }
+}
